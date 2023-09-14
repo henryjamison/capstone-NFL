048686f9ed18024091717c91cb3119ad69b6d2aa
Marshallnj1 <marshallnj1@appstate.edu>
2023-09-14 16:43:14 -0400

    search bar and background

--- hello.py
+++ hello.py
@@ -23,6 +23,26 @@ def render_tables(year=None):
     return render_template('tables.html',  tables=[df.to_html(classes='data myTable')], titles=df.columns.values, years=years)
 
 @app.route('/home')
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def render_home():
+    if request.method == 'POST':
+        # Retrieve the text from the textarea
+        text = request.form.get('textarea')
+        get_player(text)
     return render_template('home.html')
+
+def get_player(name):
+    df = pd.read_csv('2022_fantasy.csv')
+    df['Player'] = df['Player'].apply(lambda x: x.split('*')[0]).apply(lambda x: x.split('\\')[0])
+    player = df[df['Player'] == name]
+    if (player.empty):
+        print('No such player')
+    else:
+        id = str(player['PlayerID'][0])
+        get_fant_table(id)
+
+def get_fant_table(player_id):
+    url = 'https://www.pro-football-reference.com/players/' + player_id[0] + '/' + player_id + '/' + 'fantasy/2022/'
+    table = pd.read_html(url)[0]
+    print(table.head())
+
